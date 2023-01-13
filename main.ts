28be06f1f5adaab34b159ef8148deb239025714b
Diyorber <122543555+Diyorber@users.noreply.github.com>
2023-01-13 09:55:23 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.B, function () {
-    music.playMelody("F A C F D B G C ", 5000000000)
+    music.playMelody("C B D G B D C A ", 174)
     basic.showIcon(IconNames.Sword)
 })
 basic.showIcon(IconNames.Heart)
