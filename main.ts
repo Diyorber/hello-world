4e4540f013d0446b896d5b25db7de90b26e41a92
Diyorber <122543555+Diyorber@users.noreply.github.com>
2023-01-19 09:44:15 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,22 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Happy)
 })
 input.onButtonPressed(Button.B, function () {
-    music.playMelody("C B D G B D C A ", 174)
-    basic.showIcon(IconNames.Sword)
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Sword)
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Yes)
+        basic.showIcon(IconNames.Rollerskate)
+        music.playMelody("A G F G A F G - ", 174)
+        music.playMelody("G F G - G F G - ", 176)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.twinkle), SoundExpressionPlayMode.InBackground)
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.StickFigure)
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.Happy)
+    }
 })
 basic.showIcon(IconNames.Heart)
